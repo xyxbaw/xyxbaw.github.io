1b68f453f33d215b42a8a6c5e7a771cb591d4ff7
xyxbaw <kiseph1024@gmail.com>
2024-12-10 16:48:37 +0800

    Site updated: 2024-12-10 16:48:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/09/hello-world/","2024/12/10/这是一篇新的博文/","2024/12/10/My-New-Post/"];function toRandomPost(){
+var posts=["2024/12/10/这是一篇新的博文/","2024/12/10/My-New-Post/","2024/12/09/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
