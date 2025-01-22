8a9b93214dd5eaa2062864714bbe2ed6ce314560
xyxbaw <kiseph1024@gmail.com>
2025-01-22 16:24:37 +0800

    Site updated: 2025-01-22 16:24:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["My-New-Post/","hello-world/","这是一篇新的博文/"];function toRandomPost(){
+var posts=["这是一篇新的博文/","My-New-Post/","hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
