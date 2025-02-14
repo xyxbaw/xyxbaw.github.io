1d2fcc9e95e52f897ec29ee7d642bcaccdef823c
xyxbaw <kiseph1024@gmail.com>
2025-02-14 16:45:04 +0800

    Site updated: 2025-02-14 16:45:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["something/","something-2/","1/"];function toRandomPost(){
+var posts=["something/","something-2/","1/","post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
