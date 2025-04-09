9e3471b9212cf129addb4709b0b312dd576eddcf
xyxbaw <kiseph1024@gmail.com>
2025-04-09 11:39:46 +0800

    Site updated: 2025-04-09 11:39:45

---
+++ json/music.js
@@ -0,0 +1,8 @@
+{
+    "name": "UP",
+    "artist": "KARINA",
+    "url": "https://cdn.jsdelivr.net/npm/karina-up@1.0.3/KARINA-UP.mp3",
+    "cover": "https://cdn.jsdelivr.net/npm/karina-up@1.0.3/cover.jpg",
+    "lrc": ""
+  }
+  
